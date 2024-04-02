5c789a6507ece08372fd856efc39f9d84ca63144
JonahtanCF <jonathanf439@gmail.com>
2024-04-01 22:23:08 -0300

    pool conexao Supabase desabilitado

--- src/services/produtos/FindAllProdutosService.ts
+++ src/services/produtos/FindAllProdutosService.ts
@@ -2,9 +2,18 @@ import prismaClient from "../../prsima";
 
 class FindAllProdutosService {
   async execute() {
-    const allProdutos = await prismaClient.produto.findMany();
+    try {
+      // Limpar os prepared statements existentes
+      await prismaClient.$executeRaw`DEALLOCATE ALL`;
 
-    return allProdutos;
+      // Executar a consulta para encontrar todos os produtos
+      const allProdutos = await prismaClient.produto.findMany();
+
+      return allProdutos;
+    } catch (error) {
+      // Lidar com erros de forma apropriada
+      throw new Error(`Erro ao buscar produtos: ${error.message}`);
+    }
   }
 }
 
--- src/services/users/AuthUserService.ts
+++ src/services/users/AuthUserService.ts
@@ -10,7 +10,7 @@ interface AuthRequest {
 class AuthUserService {
   async execute({ email, password }: AuthRequest) {
     //Verificar se email existe
-    const user = await prismaClient.user.findFirst({
+    const user = await prismaClient.user.findUnique({
       where: {
         email: email,
       },
